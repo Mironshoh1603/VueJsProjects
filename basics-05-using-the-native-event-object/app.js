6c81f97ab24bc7ae0d2a4639c25d72a5eb60532e
Mironshoh1603 <mironshohasadov1603@gmail.com>
2022-07-25 13:08:34 +0500

    watch computed methods between defference

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -3,15 +3,36 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      fullName: "",
     };
   },
   methods: {
+    setName(val) {
+      this.name = val.target.value;
+    },
+    outputFullname(val) {
+      return this.name + val.target.value;
+    },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+  },
+  computed: {
+    renderFullname() {
+      console.log("ishladi");
+      return this.name + "Asadov";
+    },
+    renderCouter() {
+      return this.counter;
+    },
+  },
+  watch: {
+    name(newData, oldData) {
+      return newData + " Asadov";
     },
   },
 });
